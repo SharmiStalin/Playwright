8ee6e845bce3d526aa05e1d6843fea584ef53eb2
SharmiStalin <sharmistravenkateswar@gmail.com>
2024-10-03 10:20:05 +0100

    Update webpagelogin.spec.js

--- webpagelogin.spec.js
+++ webpagelogin.spec.js
@@ -18,5 +18,10 @@ console.log(`The url of the page is ${url}`);
 //Get the title of the page
 const title = browserPage.title();
 console.log(`The title of the page is ${title}`);
+await browserPage.waitForTimeout(3000);
 
+    //Close the page, context and browser instance
+    await browserPage.close();
+    await browserContext.close();
+    await browserLaunch.close();
 })
