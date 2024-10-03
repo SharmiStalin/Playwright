f4b7d8956a372c9ec4bd789b26bf99e5db2843d5
SharmiStalin <sharmistravenkateswar@gmail.com>
2024-10-03 09:13:18 +0100

    Add files via upload

---
+++ webpagelogin.spec.js
@@ -0,0 +1,22 @@
+//Task is to print the title and url of a web page using Playwright
+
+import {chromium, test} from "@playwright/test";
+
+test(`Launch, load website and print the title`, async () => {
+
+//Launch Chromium in non-headless mode
+const browserLaunch = await chromium.launch({headless : false});
+//Create a new browser context
+const browserContext = await browserLaunch.newContext();
+//Open a new page within the browser context.
+const browserPage = await browserContext.newPage();
+//Load the url https://login.salesforce.com/
+await browserPage.goto("https://login.salesforce.com");
+ //Get the url of the page
+const url = browserPage.url();
+console.log(`The url of the page is ${url}`);
+//Get the title of the page
+const title = browserPage.title();
+console.log(`The title of the page is ${title}`);
+
+})
